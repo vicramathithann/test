0fb52a7c0e39d841a80180ce6287d769a7467318
vicram <165933620+vicramathithann@users.noreply.github.com>
2025-02-01 14:19:44 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class demo{
+    public static void main (String []args){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter the number:");
+
+        int numb =sc.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(numb+"X"+i+"="+(i*numb));
+
+        }
+    }
+}
